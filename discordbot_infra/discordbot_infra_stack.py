eaf14f8a4b3ba28135e2661d473892391683dcf0
Andrew Ha <andrew.ha@live.com.au>
2021-10-23 16:03:58 +1100

    Importing secrets for discord API token

--- discordbot_infra/discordbot_infra_stack.py
+++ discordbot_infra/discordbot_infra_stack.py
@@ -1,13 +1,19 @@
 from aws_cdk import core as cdk
 from aws_cdk import aws_ecs as ecs
-
+from aws_cdk import aws_secretsmanager as secretsmanager
+from aws_cdk import aws_
 
 class DiscordbotInfraStack(cdk.Stack):
 
   def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
     super().__init__(scope, construct_id, **kwargs)
 
-    # The code that defines your stack goes here
-    # Create ECS cluster
-    cluster = ecs.Cluster(self, "Cluster")
+    # Importing existing resources to CDK
+    DiscordAuthTokenSecretArn = "arn:aws:secretsmanager:ap-southeast-2:811385878444:secret:/DiscordBot/Discord/AuthToken"
+    DiscordAuthTokenSecret = secretsmanager.Secret.from_secret_partial_arn(self, "DiscordBotTokenSecret", DiscordAuthTokenSecretArn)
 
+    # Lambda function to respond to Discord's API calls
+    # DiscordBotResponderLambda = 
+
+    # ECS cluster to run server traffic
+    cluster = ecs.Cluster(self, "Cluster")
