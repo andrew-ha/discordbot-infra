2147bb0d2531853642e350865d33d35789149b5c
Andrew Ha <andrew.ha@live.com.au>
2021-12-05 21:46:48 +1100

    new nodejs version for cdk 2.0

--- discordbot_infra/discordbot_infra_stack.py
+++ discordbot_infra/discordbot_infra_stack.py
@@ -2,6 +2,7 @@
 from constructs import Construct
 from aws_cdk import App, Stack                    # core constructs
 from aws_cdk import aws_ecs as ecs
+from aws_cdk import aws_ec2 as ec2
 from aws_cdk import aws_secretsmanager as secretsmanager
 # from aws_cdk import aws_
 
@@ -17,5 +18,17 @@ class DiscordbotInfraStack(Construct):
     # Lambda function to respond to Discord's API calls
     # DiscordBotResponderLambda = 
 
+    # VPC to deploy ECS related infra
+    vpc = ec2.Vpc(self, "VPC",
+        cidr="10.0.0.0/16"
+    )
+
+    # Iterate the private subnets
+    selection = vpc.select_subnets(
+        subnet_type=ec2.SubnetType.PUBLIC
+    )
+
     # ECS cluster to run server traffic
-    cluster = ecs.Cluster(self, "Cluster")
+    cluster = ecs.Cluster(self, "Cluster",
+      vpc=vpc 
+    )
